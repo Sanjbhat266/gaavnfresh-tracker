d3e87b04b077a7bd45dd922d78d1aeff0f2311f6
Sanjbhat266 <consultsb6@gmail.com>
2026-07-13 08:22:55 +0530

    Create sw-stock.js

---
+++ stock/sw-stock.js
@@ -0,0 +1,15 @@
+var CACHE = 'gaavn-stock-v1';
+self.addEventListener('install', function(e){ self.skipWaiting(); });
+self.addEventListener('activate', function(e){
+  e.waitUntil(caches.keys().then(function(keys){ return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);})); }));
+  self.clients.claim();
+});
+self.addEventListener('fetch', function(e){
+  var url = e.request.url;
+  if(url.indexOf('script.google.com')>=0){ return; }
+  if(url.indexOf('.html')>=0 || url.endsWith('/') ){
+    e.respondWith(fetch(e.request,{cache:'no-store'}).catch(function(){ return caches.match(e.request); }));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(function(c){ return c || fetch(e.request); }));
+});
