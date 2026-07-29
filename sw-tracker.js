4de13f355ccbc5e6ba9835e03f057c32da77354a
Sanjbhat266 <consultsb6@gmail.com>
2026-07-29 20:42:37 +0530

    Update sw-tracker.js

--- sw-tracker.js
+++ sw-tracker.js
@@ -1,4 +1,4 @@
-var CACHE='gaavn-tracker-v156';
+var CACHE='gaavn-tracker-v157';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(keys){ return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);})); }));
