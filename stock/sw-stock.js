4b605303a8a4e96e018387546f0064ad85e087df
Sanjbhat266 <consultsb6@gmail.com>
2026-08-23 09:58:37 +0530

    Update sw-stock.js

--- stock/sw-stock.js
+++ stock/sw-stock.js
@@ -1,4 +1,4 @@
-var CACHE = 'gaavn-stock-v5';
+var CACHE = 'gaavn-stock-v6';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(keys){ return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);})); }));
